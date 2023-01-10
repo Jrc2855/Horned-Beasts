8cbe14b85db4b283d94684dbe1857e6a5edba04f
John <jrc2855@gmail.com>
2023-01-10 04:18:43 +0000

    Horned Beasts Lab 01 Finished

---
+++ 301/horned-beasts/src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+  render() {
+    return (
+      <>
+        <p>Author: John Chavez</p>
+      </>
+    )
+  }
+}
+
+export default Footer;
